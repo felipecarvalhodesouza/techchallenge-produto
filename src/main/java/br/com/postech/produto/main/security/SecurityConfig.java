3587dd250d96062ad51414b38ef2e0eece5ad523
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2024-07-13 01:22:09 -0300

    Adicionando segurança na API

---
+++ src/main/java/br/com/postech/produto/main/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package br.com.postech.produto.main.security;
+
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    URL jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> 
+            	csrf.disable()
+            )
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                	.requestMatchers(HttpMethod.GET).permitAll()
+                    .anyRequest().hasAnyAuthority("ROLE_ADMIN")
+            )
+           .oauth2ResourceServer((oauth2) -> oauth2.jwt( jwt -> {
+        	   jwt.decoder(jwtDecoder());
+        	   jwt.jwtAuthenticationConverter(jwtAuthenticationConverter());
+           	}))
+           .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+    
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String jwkSetUri = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_pGFsCFVuS/.well-known/jwks.json";
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+    
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return converter;
+    }
+}
